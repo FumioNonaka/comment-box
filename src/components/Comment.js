5230c11858fa1fa187b9302decf2b7983f7ed543
野中 文雄 <fumio.nonaka@IT-04357-M.local>
2018-12-26 13:01:37 +0900

    Using markdown

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import Remarkable from 'remarkable';
 
 class Comment extends Component {
+    rawMarkup() {
+        const markDown = new Remarkable();
+        const rawMarkup = markDown.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
 	render() {
 		return (
 			<div className="Comment">
 				<h2 className="commentAuthor">
 				{this.props.author}
 				</h2>
-				{this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
 			</div>
 		);
 	}
