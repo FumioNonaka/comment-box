9d1e47def5af166ab61eade28599c4deaf58857c
野中 文雄 <fumio.nonaka@IT-04357-M.local>
2018-12-27 14:25:03 +0900

    Saving data to localStorage

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -5,36 +5,49 @@ class CommentForm extends Component {
 		super(props);
 		this.state = {author: '', text: ''};
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleAuthorChange = this.handleAuthorChange.bind(this);
-		this.handleTextChange = this.handleTextChange.bind(this);
+		// this.handleAuthorChange = this.handleAuthorChange.bind(this);
+		// this.handleTextChange = this.handleTextChange.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
-	handleAuthorChange(eventObject) {
+	/* handleAuthorChange(eventObject) {
 		this.setState({author: eventObject.target.value});
 	}
 	handleTextChange(eventObject) {
 		this.setState({text: eventObject.target.value});
+	} */
+	handleChange(eventObject) {
+		const input = eventObject.target;
+		const state = {};
+		state[input.id] = input.value;
+		this.setState(state);
 	}
 	handleSubmit(eventObject) {
 		eventObject.preventDefault();
-		const author = this.state.author;
-		const text = this.state.text;
-		console.log(author, text);  // 確認用
+		const author = this.state.author.trim();
+		const text = this.state.text.trim();
+		if (!author || !text) {
+			this.setState({author: author, text: text});
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
 		this.setState({author: '', text: ''});
 	}
 	render() {
 		return (
 			<form className="CommentForm"  onSubmit={this.handleSubmit}>
 				<input
+					id="author"
 					type="text"
 					placeholder="名前"
 					value={this.state.author}
-					onChange={this.handleAuthorChange}
+					onChange={this.handleChange}
 				/>
 				<input
+					id="text"
 					type="text"
 					placeholder="コメントを入力"
 					value={this.state.text}
-					onChange={this.handleTextChange}
+					onChange={this.handleChange}
 				/>
 				<input type="submit" value="送信" />
 			</form>
